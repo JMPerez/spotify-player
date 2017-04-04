13f1e2070fd5c973c42df43865a12919831d90f4
José M. Pérez <jperez@spotify.com>
2017-04-04 22:28:54 +0200

    Adding generic calls + /me

--- public/spotify-player.js
+++ public/spotify-player.js
@@ -3,6 +3,7 @@ class SpotifyPlayer {
         this.options = options;
         this.listeners = {};
         this.accessToken = null;
+        this.loggedIn = false;
     }
 
     on(eventType, callback) {
@@ -11,16 +12,21 @@ class SpotifyPlayer {
     }
 
     init() {
-        // read a cookie with the access token
-        // if (cookie) {
-            // assume logged in
-            // schedule a refresh
-        // }
-        this.login().then(accessToken => {
+        const loop = () => {
+            this.fetchPlayer().then(data => {
+                this.dispatch('update', data);
+            });
+        };
+
+        return this.login().then(accessToken => {
             this.accessToken = accessToken;
-            setInterval(this.fetchPlayer.bind(this), 1500);
-            this.fetchPlayer();
-        });
+            this.loggedIn = true;
+            this.fetchUser().then(user => {
+                this.dispatch('login', user);
+                    setInterval(loop.bind(this), 1500);
+                    loop();
+                });
+            });
     }
 
     login() {
@@ -61,19 +67,28 @@ class SpotifyPlayer {
         return this.accessToken;
     }
 
+    fetchGeneric(url) {
+        return fetch(url, {
+            headers: { 'Authorization': 'Bearer ' + this.accessToken }
+        });
+    }
+
     fetchPlayer() {
-        fetch('https://api.spotify.com/v1/me/player',
-            {
-                headers: { 'Authorization': 'Bearer ' + this.accessToken }
-            }
-        ).then(data => data.json())
-         .then(data => {
-             const listeners = this.listeners.update;
-             if (listeners) {
-                 listeners.forEach(listener => {
-                     listener.call(null, data);
-                 });
-             }
-         });
+        return this.fetchGeneric('https://api.spotify.com/v1/me/player')
+            .then(data => data.json())
+    }
+
+    fetchUser() {
+        return this.fetchGeneric('https://api.spotify.com/v1/me')
+            .then(data => data.json())
+    }
+
+    dispatch(topic, data) {
+        const listeners = this.listeners['topic'];
+        if (listeners) {
+            listeners.forEach(listener => {
+                listener.call(null, data);
+            });
+        }
     }
 }
